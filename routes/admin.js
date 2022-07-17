e4e21385eba84cc949e39365acda94ad231de907
A3TNIP <maharjanaaju@gmail.com>
2022-07-17 22:29:01 +0545

    06-02 Completed
    Changed HTML and CSS Files
    shared data across different requests

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
 
 //      /admin/add-product => GET
 router.get('/add-product',(req, res, next) => {
@@ -14,10 +15,11 @@ router.get('/add-product',(req, res, next) => {
 
 //      /admin/add-product => POST
 router.post('/add-product', (req,res,next) => {
+    products.push({title: req.body.title});
     console.log(req.body);              // Prints the JSON Object
     console.log(req.body.title);        // Prints the title property of the JSON Object
     res.redirect('/');
 });
 
-module.exports = router;
-
+exports.routes = router;
+exports.products = products;
