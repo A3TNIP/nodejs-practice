bc8efc60a133c92b4f247401c64c3b1f35ebb505
A3TNIP <maharjanaaju@gmail.com>
2022-07-17 19:40:05 +0545

    05-08 Complete

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/',(req,res,next) => {
     console.log("This always runs");
@@ -11,8 +12,16 @@ app.use('/',(req,res,next) => {
 
 app.use('/add-product',(req, res, next) => {
     console.log("In another middleware!");
-    res.send('<h1>The "Add product" page</h1>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button</form>')
 });
+
+app.use('/product', (req,res,next) => {
+    console.log(req.body);              // Prints the JSON Object
+    console.log(req.body.title);        // Prints the title property of the JSON Object
+    res.redirect('/');
+});
+
+
 app.use('/',(req, res, next) => {
     console.log("In another middleware!");
     res.send('<h1>Hello from express</h1>')
