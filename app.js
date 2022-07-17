c5fd77a71f5c67d92aac82ca68983b9df606e817
A3TNIP <maharjanaaju@gmail.com>
2022-07-17 19:28:11 +0545

    05-07

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require('express');
 
 const app = express();
 
+app.use('/',(req,res,next) => {
+    console.log("This always runs");
+    next();
+});
+
 app.use('/add-product',(req, res, next) => {
     console.log("In another middleware!");
     res.send('<h1>The "Add product" page</h1>')
