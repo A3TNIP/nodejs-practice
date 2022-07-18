4f6ffa37f7944aa8cd9a7ec0592860f5519d17aa
A3TNIP <maharjanaaju@gmail.com>
2022-07-19 02:15:04 +0545

    implemented handlebars for 404 page

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const { engine } = require ('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', engine({
+    extname: "hbs",
+    defaultLayout: false,
+    layoutsDir: "views/layouts/"
+}));
+app.set('view engine', 'hbs');
 app.set('views', 'views')
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
